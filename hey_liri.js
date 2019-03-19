b3d351664d0c0b6d0adc357c361ee2d9bdddda3d
imonik <mk.santb@gmail.com>
2019-03-19 13:50:01 -0700

    added final functionality

--- hey_liri.js
+++ hey_liri.js
@@ -4,29 +4,42 @@ const Spotify = require('node-spotify-api');
 const axios = require('axios')
 const moment = require('moment');
 const readline = require('readline');
+const fs = require('fs');
+var option = "";
 
 moment().format();
 var spotify = new Spotify(keys.spotify);
 
-// var spotify = new Spotify({
-//     id: process.env.SPOTIFY_ID,
-//     secret: process.env.SPOTIFY_SECRET
-// });
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+
+function askUser(question) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    rl.question(question, (answer) => { 
+        console.log(answer);
+        if(answer.length > 0 && answer !== ""){
+            performActions(answer);
+        }else {
+            askUser(question);
+        }
+        
+        rl.close();
+      });
+}
 
 var mainActions = "\t* concert-this  <artist/band name here>\n \t* spotify-this-song <song name here>\n \t* movie-this <movie name here>\n \t* do-what-it-says\n" 
-  //Initial Greetings
 
+//Initial Greetings
 askUser("Hello! What can I do for you? \n Here are some options: \n " + mainActions);
  
 function performActions(answer) {
     let arr =  answer.split(" ");
     let action  = arr[0].toLowerCase();
     let criteria = arr.slice(1).join(" ");
+    logSearch("\n" + answer + "\n");
 
     switch (action) {               
         case 'concert-this':
@@ -39,21 +52,19 @@ function performActions(answer) {
             searchMovie(criteria);
             break;
         case 'do-what-it-says':
-            console.log("look in spotify");
+            random();
             break;
         default:
             break;
     }
  }
 
-
-  function askUser(question) {
-    rl.question(question, (answer) => { 
-        console.log("primero aqui " + answer);
+ function random(){
+     fs.readFile("./random.txt", "utf8", function(err, data){
+        let answer = data.replace(",", " ");
         performActions(answer);
-        rl.close();
-      });
-  }
+     });
+ }
 
 function searchOnSpotify(search){
 
@@ -61,12 +72,14 @@ spotify.search({ type: 'track', query: search }, function(err, data) {
     if (err) {
         return console.log('Error occurred: ' + err);
     }
-    console.log("--------THE SONGs--------")
-    console.log(data.tracks.items[0].album); 
     let arr = data.tracks.items;
     for(var i = 0;  i < arr.length; i++){
-        console.log(`Artist:  ${arr[i].album.artists[0].name} Album: ${arr[i].album.name}, ${arr[i].album.external_urls.spotify}`); //${arr[i].items.artist}, ${arr[i].items.name}, ${arr[i].items.preview_url}\n` );
+        console.log(`Artist:  ${arr[i].album.artists[0].name} Song: ${search}, ${arr[i].album.external_urls.spotify}, Album: ${arr[i].album.name}`);
+        logSearch(`Artist:  ${arr[i].album.artists[0].name} Song: ${search}, ${arr[i].album.external_urls.spotify}, Album: ${arr[i].album.name}\n`);
     }
+    
+    console.log("   ");
+    askUser(mainActions);
     });
 }
 
@@ -74,9 +87,14 @@ function searchMovie(movie){
   // Performing a GET request
   axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDBKEY}&t=${movie}`)
   .then(function(response){
-    console.log(response.data); // ex.: { user: 'Your User'}
-    console.log(response.status); // ex.: 200
-  });  
+    // console.log(response.data); // ex.: { user: 'Your User'}
+    // console.log(response.status); // ex.: 200
+    console.log(`Title: ${response.data.Title}\nYear: ${response.data.Year}\nIMDB: ${response.data.Ratings[0].Value}\nRotten Tomatoes: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}`);
+    logSearch(`Title: ${response.data.Title}\nYear: ${response.data.Year}\nIMDB: ${response.data.Ratings[0].Value}\nRotten Tomatoes: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}\n`);
+    console.log(" ");
+    askUser(mainActions);
+});  
+ 
 }
 
 function searchBand(band){
@@ -92,7 +110,16 @@ function searchBand(band){
 
       for(var i = 0;  i < arr.length; i++){
           console.log(`Venue: ${arr[i].venue.name} ${arr[i].venue.city}, ${arr[i].venue.country} at ${moment(arr[i].datetime).format('LLLL')}\n` );
+          logSearch(`Venue: ${arr[i].venue.name} ${arr[i].venue.city}, ${arr[i].venue.country} at ${moment(arr[i].datetime).format('LLLL')}\n`);
+          
       }
-
+      console.log("   ");
+      askUser(mainActions);
     });  
+  }
+
+  function logSearch(content){
+    fs.appendFile('log.txt', content, function (err) {
+        if (err) throw err;
+      }); 
   }
